8169dbd4f4acae447fca233d1fcd8a29d6ad7207
Yannic Kilcher <yk@users.noreply.github.com>
2022-04-16 22:53:37 +0200

    added parameters

--- web-playground/pages/api/complete.js
+++ web-playground/pages/api/complete.js
@@ -6,16 +6,16 @@ const { serverRuntimeConfig } = getConfig();
 export default async function handler(req, res) {
   if (req.method == "POST") {
     const {
-      body: { prompt },
+      body
     } = req;
-    if (!prompt) {
+    if (!body?.prompt) {
       res.statusCode = 400;
       res.end("Missing prompt");
       return;
     }
     const { apiUrl } = serverRuntimeConfig;
     try {
-      const { data } = await axios.post(`${apiUrl}/complete`, { prompt });
+      const { data } = await axios.post(`${apiUrl}/complete`, body );
       res.status(200).json(data);
     } catch (error) {
       res.status(500).json({ error: error.message });
